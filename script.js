ae120731b20f3ac8487b4f15672389a65136e495
LukeBucciarelli <luke.bucciarelli@gmail.com>
2015-11-19 12:57:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+ 
+	var $body = $('body');
+
+  alert("let's play FizzBuzz!");  
+ 
+
+// my fizzbuzz program
+
+	for (var i = 0; i <= 100; i++) {
+
+		if (i % 3 == 0 && i % 5 == 0) {
+			$body.append('<div>FizzBuzz</div>');
+		}
+		else if (i % 3 == 0) {
+			$body.append('<div>Fizz</div>');
+		}
+		else if (i % 5 == 0) {
+			$body.append('<div>Buzz</div>');
+		}
+		else {
+			$body.append('<div>' + i + '</div>');
+		}
+	}
+});
+
+
+
